970771a720a64056e399294a4443fd54785aa87f
bmaudet <maudet.baptiste@gmail.com>
2015-12-09 10:22:36 +0100

    V1

---
+++ winphone-listener/Service/BiblioMobile/Models/BookDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace BiblioMobile.Models
+{
+    public class BookDetails
+    {
+        public String id { get; set; }
+        public List<votes> votes { get; set; }
+    }
+
+    public class votes
+    {
+        public int choix { get; set; }
+        public String prenom { get; set; }
+    }
+}
