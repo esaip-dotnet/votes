e2bce1a70cb618dddf3c9fef42d546c35867fdb6
ezria <corentinchevallier@gmail.com>
2015-12-11 12:28:52 +0100

    -Add of GUI test, no test done.

---
+++ leap-sender/VoteByLeapMotionProject/LeapMotionVoteSystemWindow.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace VoteByLeapMotionProject
+{
+    partial class LeapMotionVoteSystemWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.nextElection = new System.Windows.Forms.Button();
+            this.previousElection = new System.Windows.Forms.Button();
+            this.addElection = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(270, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(237, 25);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Leap Motion Vote System";
+            // 
+            // nextElection
+            // 
+            this.nextElection.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextElection.Location = new System.Drawing.Point(651, 199);
+            this.nextElection.Name = "nextElection";
+            this.nextElection.Size = new System.Drawing.Size(101, 23);
+            this.nextElection.TabIndex = 1;
+            this.nextElection.Text = "-->";
+            this.nextElection.UseVisualStyleBackColor = true;
+            this.nextElection.Click += new System.EventHandler(this.nextElection_Click);
+            // 
+            // previousElection
+            // 
+            this.previousElection.Location = new System.Drawing.Point(12, 199);
+            this.previousElection.Name = "previousElection";
+            this.previousElection.Size = new System.Drawing.Size(101, 23);
+            this.previousElection.TabIndex = 2;
+            this.previousElection.Text = "<--";
+            this.previousElection.UseVisualStyleBackColor = true;
+            this.previousElection.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // addElection
+            // 
+            this.addElection.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addElection.Location = new System.Drawing.Point(651, 12);
+            this.addElection.Name = "addElection";
+            this.addElection.Size = new System.Drawing.Size(101, 70);
+            this.addElection.TabIndex = 3;
+            this.addElection.Text = "Ajouter une élection";
+            this.addElection.UseVisualStyleBackColor = true;
+            this.addElection.Click += new System.EventHandler(this.addElection_Click);
+            // 
+            // LeapMotionVoteSystemWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 417);
+            this.Controls.Add(this.addElection);
+            this.Controls.Add(this.previousElection);
+            this.Controls.Add(this.nextElection);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "LeapMotionVoteSystemWindow";
+            this.Text = "Leap Motion Vote System Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button nextElection;
+        private System.Windows.Forms.Button previousElection;
+        private System.Windows.Forms.Button addElection;
+
+    }
+}
---
+++ leap-sender/VoteByLeapMotionProject/PanelElection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteByLeapMotionProject
+{
+    class PanelElection
+    {
+            private System.Windows.Forms.Panel panelElection;
+            private System.Windows.Forms.Label labelElection;
+            private System.Windows.Forms.Button buttonResetElection;
+            private System.Windows.Forms.Button buttonGoElection;
+        /// <summary>
+        /// Class allowing to add a panel containing the data and the button for the election on the systeme votes
+        /// </summary>
+        /// <param name="idElection"></param>
+        public PanelElection(Election election)
+        {
+            this.panelElection = new System.Windows.Forms.Panel();
+            this.labelElection = new System.Windows.Forms.Label();
+            this.buttonResetElection = new System.Windows.Forms.Button();
+            this.buttonGoElection = new System.Windows.Forms.Button();
+            this.panelElection.SuspendLayout();
+
+            this.panelElection.Controls.Add(this.labelElection);
+            this.panelElection.Controls.Add(this.buttonResetElection);
+            this.panelElection.Controls.Add(this.buttonGoElection);
+            this.panelElection.Location = new System.Drawing.Point(305, 157);
+            this.panelElection.Name = "panelElection";
+            this.panelElection.Size = new System.Drawing.Size(159, 134);
+            this.panelElection.TabIndex = 4;
+            // 
+            // labelElection
+            // 
+            this.labelElection.AutoSize = true;
+            this.labelElection.Location = new System.Drawing.Point(63, 33);
+            this.labelElection.Name = "labelElection";
+            this.labelElection.Size = new System.Drawing.Size(35, 13);
+            this.labelElection.TabIndex = 2;
+            this.labelElection.Text = election.nom;
+            // 
+            // buttonResetElection
+            // 
+            this.buttonResetElection.Location = new System.Drawing.Point(43, 108);
+            this.buttonResetElection.Name = "buttonResetElection";
+            this.buttonResetElection.Size = new System.Drawing.Size(75, 23);
+            this.buttonResetElection.TabIndex = 1;
+            this.buttonResetElection.Text = "Réinitialiser les votes de cette élection";
+            this.buttonResetElection.UseVisualStyleBackColor = true;
+            // 
+            // buttonGoElection
+            // 
+            this.buttonGoElection.Location = new System.Drawing.Point(43, 79);
+            this.buttonGoElection.Name = "buttonGoElection";
+            this.buttonGoElection.Size = new System.Drawing.Size(75, 23);
+            this.buttonGoElection.TabIndex = 0;
+            this.buttonGoElection.Text = "Démarrer le système de vote pour cette élection";
+            this.buttonGoElection.UseVisualStyleBackColor = true;
+            this.panelElection.ResumeLayout(false);
+            this.panelElection.PerformLayout();
+        }
+    }
+}
